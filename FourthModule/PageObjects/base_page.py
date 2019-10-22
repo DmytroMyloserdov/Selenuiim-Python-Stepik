9b3622f43bc61a8a9f95566811d5cbb1d80bb4aa
Dmytro Myloserdov <dmytro.myloserdov@onseo.biz>
2019-10-22 17:12:47 +0300

    Added base method for checking element

--- FourthModule/PageObjects/base_page.py
+++ FourthModule/PageObjects/base_page.py
@@ -1,9 +1,19 @@
 from selenium.webdriver.chrome.webdriver import WebDriver 
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePageObject():
-    def __init__(self, browser: WebDriver, url: str):
+    def __init__(self, browser: WebDriver, url: str, timeout = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_exists(self, by_selector: By, selector: str):
+        try:
+            self.browser.find_element(by_selector, selector)
+            return True
+        except (NoSuchElementException):
+            return False
--- FourthModule/PageObjects/main_page.py
+++ FourthModule/PageObjects/main_page.py
@@ -5,3 +5,6 @@ class MainPageObject(BasePageObject):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_exists(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
